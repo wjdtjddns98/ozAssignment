af6af8f816ddaf91aeba4c9d28d6f008f578c4f6
wjdtjddns98 <wjdtjddns98@naver.com>
2025-04-24 17:19:16 +0900

    과제제출 Day11

---
+++ Day11/mymath.py
@@ -0,0 +1,16 @@
+import math
+
+def triangle_area(base, height):
+    return (base * height) / 2
+
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+def rectangle_area(width, length):
+    return width * length
+
+def cuboid_volume(width, length, height):
+    return width * length * height
+
+def cylinder_volume(radius, height):
+    return math.pi * radius ** 2
